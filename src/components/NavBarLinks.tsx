f39cf5aafb18b4d323368bc6d68d1b64ba6f2e82
niiko <nicolas.rocagel@gmail.com>
2023-09-01 11:50:56 +0200

    add shop icon on navbar

--- src/components/NavBarLinks.tsx
+++ src/components/NavBarLinks.tsx
@@ -4,10 +4,14 @@ import { usePathname } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useStore } from "@/store";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function NavBarLinks({ isAdmin }: { isAdmin: boolean }) {
   const pathname = usePathname();
   const [isAdminSet, setIsAdmin] = useState(false);
+  const { bag, isOpen } = useStore();
 
   useEffect(() => {
     setIsAdmin(isAdmin);
@@ -29,18 +33,40 @@ export default function NavBarLinks({ isAdmin }: { isAdmin: boolean }) {
         >
           Buy
         </Link>
-        <Link className="ml-2" href="/contact">
-          <Image src="/mail-icon.png" width={40} height={40} alt="Contact" />
+        {bag.length > 0 && (
+          <button
+            onClick={() => {
+              useStore.setState({ isOpen: true });
+            }}
+            className="-translate-y-[4px] ml-1"
+          >
+            {" "}
+            {/* <FontAwesomeIcon width="24" icon={faBagShopping} /> */}
+            <Image
+              src="/shop2.png"
+              width={30}
+              height={30}
+              alt="Shopping bag"
+            ></Image>
+          </button>
+        )}
+        <Link className="ml-[16px] -translate-y-[5.6px]" href="/contact">
+          <Image
+            src={pathname == "/contact" ? "/mail.png" : "/mail2.png"}
+            width={25}
+            height={25}
+            alt="Contact"
+          />
         </Link>
         <Link
-          className="ml-3 translate-y-[2px]"
+          className="ml-[20px] -translate-y-[6px]"
           target="blank"
           href="https://www.instagram.com/andreagracerocagel"
         >
           <Image
-            src="/insta-icon.png"
-            width={40}
-            height={40}
+            src="/insta2.png"
+            width={25}
+            height={25}
             alt="Instagram link"
           />
         </Link>
