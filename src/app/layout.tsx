da8811099867380d976770f07576824ef9c31635
niiko <nicolas.rocagel@gmail.com>
2023-09-06 17:34:55 +0200

    testing env cloudinary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,9 @@ import { Inter, Roboto_Mono, Tilt_Prism, Tenor_Sans } from "next/font/google";
 import Script from "next/script";
 import NavBar from "../components/NavBar";
 import CartDrawer from "../components/CartDrawer";
-import DrawerOverlay from "../components/DrawerOverlay";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
@@ -55,17 +53,13 @@ export default async function RootLayout({
         <body className={inter.className}>
           <NavBar />
           <CartDrawer />
-          {/* <DrawerOverlay /> */}
           <ToastContainer
             position="top-center"
             hideProgressBar
             theme="colored"
           />
-
-          {/* <div className="translate-y-24">{children}</div> */}
           <div>{children}</div>
           <Analytics />
-          <Script src="./main.js" />
           <Script
             defer
             src="https://kit.fontawesome.com/766e633129.js"
--- src/components/DrawerOverlay.tsx
+++
@@ -1,5 +0,0 @@
-export default function Overlay() {
-  return (
-    <div className="drawer-overlay transition-opacity opacity-0 duration-1000  translate-x-full  h-screen z-50 w-full absolute top-0 right-0 pointer-events-none overflow-x-hidden"></div>
-  );
-}
