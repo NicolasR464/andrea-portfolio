daa0ed8d99fb194a24251725c08233058fbd50ba
niiko <nicolas.rocagel@gmail.com>
2024-11-24 20:53:41 +0100

    🔨 Add await to searchParams

--- src/app/a/order/page.tsx
+++ src/app/a/order/page.tsx
@@ -1,66 +1,93 @@
-import BackBtn from "@/components/BackBtn";
-import Image from "next/image";
+import BackBtn from '@/components/BackBtn'
+import Image from 'next/image'
 
 const getOrderDetail = async (id: string) => {
-  const orderDetail = await fetch(`${process.env.DOMAIN}/api/order?id=${id}`);
+    const orderDetail = await fetch(`${process.env.DOMAIN}/api/order?id=${id}`)
 
-  const orderParsed = await orderDetail.json();
+    const orderParsed = await orderDetail.json()
 
-  return orderParsed;
-};
+    return orderParsed
+}
 
 export default async function Details({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: { id: string };
+    searchParams: Promise<{ id: string }>
 }) {
-  const orderDetail: any = await getOrderDetail(searchParams.id);
+    const { id } = await searchParams
 
-  return (
-    <article>
-      <BackBtn />
+    const orderDetail: any = await getOrderDetail(id)
 
-      <div className="flex w-screen justify-center">
-        <h1 className="text-3xl">Order details</h1>
-      </div>
-      <div className="flex items-center flex-col">
-        {orderDetail && (
-          <div className="mt-6 p-3 justify-center items-center flex flex-col b-2 border-solid border-2 rounded-xl">
-            <h4 className="font-medium underline">user details</h4>
-            <span>name: {orderDetail.order.customerDetails.name}</span>
-            <span>email: {orderDetail.order.customerDetails.email}</span>
-            <h4 className="font-medium underline">user address</h4>
-            <span>{orderDetail.order.customerDetails.address.line1}</span>
-            <span>{orderDetail.order.customerDetails.address.line2}</span>
-            <span>{orderDetail.order.customerDetails.address.postal_code}</span>
-            <span>{orderDetail.order.customerDetails.address.city}</span>
-            <span>{orderDetail.order.customerDetails.address.state}</span>
-            <span>{orderDetail.order.customerDetails.address.country}</span>
-          </div>
-        )}
-        <section className="flex flex-wrap justify-center">
-          {orderDetail &&
-            orderDetail.order.orderItems.map((order: any, i: number) => (
-              <div
-                className="m-2 mt-6 p-3 justify-center items-center flex flex-col b-2 border-solid border-2 rounded-xl"
-                key={i}
-              >
-                <h3 className="font-medium underline">order#{i + 1}</h3>
-                <span>quantity: {order.quantity} prints</span>
-                {order.imageUrl && (
-                  <Image
-                    width={200}
-                    height={200}
-                    src={order.imageUrl}
-                    alt="Drawing picture"
-                  />
+    return (
+        <article>
+            <BackBtn />
+
+            <div className="flex w-screen justify-center">
+                <h1 className="text-3xl">Order details</h1>
+            </div>
+            <div className="flex items-center flex-col">
+                {orderDetail && (
+                    <div className="mt-6 p-3 justify-center items-center flex flex-col b-2 border-solid border-2 rounded-xl">
+                        <h4 className="font-medium underline">user details</h4>
+                        <span>
+                            name: {orderDetail.order.customerDetails.name}
+                        </span>
+                        <span>
+                            email: {orderDetail.order.customerDetails.email}
+                        </span>
+                        <h4 className="font-medium underline">user address</h4>
+                        <span>
+                            {orderDetail.order.customerDetails.address.line1}
+                        </span>
+                        <span>
+                            {orderDetail.order.customerDetails.address.line2}
+                        </span>
+                        <span>
+                            {
+                                orderDetail.order.customerDetails.address
+                                    .postal_code
+                            }
+                        </span>
+                        <span>
+                            {orderDetail.order.customerDetails.address.city}
+                        </span>
+                        <span>
+                            {orderDetail.order.customerDetails.address.state}
+                        </span>
+                        <span>
+                            {orderDetail.order.customerDetails.address.country}
+                        </span>
+                    </div>
                 )}
+                <section className="flex flex-wrap justify-center">
+                    {orderDetail &&
+                        orderDetail.order.orderItems.map(
+                            (order: any, i: number) => (
+                                <div
+                                    className="m-2 mt-6 p-3 justify-center items-center flex flex-col b-2 border-solid border-2 rounded-xl"
+                                    key={i}
+                                >
+                                    <h3 className="font-medium underline">
+                                        order#{i + 1}
+                                    </h3>
+                                    <span>
+                                        quantity: {order.quantity} prints
+                                    </span>
+                                    {order.imageUrl && (
+                                        <Image
+                                            width={200}
+                                            height={200}
+                                            src={order.imageUrl}
+                                            alt="Drawing picture"
+                                        />
+                                    )}
 
-                <span>{order.name}</span>
-              </div>
-            ))}
-        </section>
-      </div>
-    </article>
-  );
+                                    <span>{order.name}</span>
+                                </div>
+                            )
+                        )}
+                </section>
+            </div>
+        </article>
+    )
 }
